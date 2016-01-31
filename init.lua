5f6f4ff995810b30d014cbab2f8a836c38fb94b1
Mark Biek <support@janustech.net>
2016-01-31 14:13:51 -0500

    Added full-screen and left/right half commands

--- init.lua
+++ init.lua
@@ -3,6 +3,57 @@ local hotkey = require "mjolnir.hotkey"
 local window = require "mjolnir.window"
 local fnutils = require "mjolnir.fnutils"
 
+
+-- CMD-OPT-f full screen
+hotkey.bind({"cmd", "alt"}, "f", function()
+    local win = window.focusedwindow()
+
+    -- Screen of the currently focused window
+    local screen = win:screen()
+    local screenFrame = screen:frame()
+
+    win:setframe(screenFrame)
+end)
+
+-- CMD-OPT-left left half
+hotkey.bind({"cmd", "alt"}, "left", function()
+    local win = window.focusedwindow()
+    local f = win:frame()
+
+    -- Screen of the currently focused window
+    local screen = win:screen()
+    local screenFrame = screen:frame()
+
+    f = screenFrame
+    f.w = f.w / 2 - 1
+
+    win:setframe(f)
+end)
+
+-- CMD-OPT-right right half
+hotkey.bind({"cmd", "alt"}, "right", function()
+    local win = window.focusedwindow()
+    local f = win:frame()
+
+    -- Screen of the currently focused window
+    local screen = win:screen()
+    local screenFrame = screen:frame()
+
+    f = screenFrame
+    f.w = f.w / 2
+    f.x = f.x + f.w - 2
+
+    win:setframe(f)
+end)
+
+-- CMD-CTRL-OPT-right next display
+hotkey.bind({"cmd", "ctrl", "alt"}, "right", function()
+end)
+
+-- CMD-CTRL-OPT-left previous display
+hotkey.bind({"cmd", "ctrl", "alt"}, "left", function()
+end)
+
 -- Position the current window in the bottom-right corner
 hotkey.bind({"cmd", "alt", "ctrl"}, "down", function()
     local win = window.focusedwindow()
